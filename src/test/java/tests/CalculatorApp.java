19e7be193f4c59d8e98fb898df80675459b0612a
mithrili <altnskihsan@gmail.com>
2024-03-05 19:10:57 +0300

    05.03.2024

--- src/test/java/tests/CalculatorApp.java
+++ src/test/java/tests/CalculatorApp.java
@@ -10,7 +10,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-public class Calculator {
+public class CalculatorApp {
     AndroidDriver<AndroidElement> driver;
     @Test
     public void deneme() throws MalformedURLException {
---
+++ src/test/java/tests/KiwiApp.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class KiwiApp {
+
+    @Test
+    public void kiwiAppTest(){
+        // Verify that the application is installed.
+        // Confirm that the application is successfully opened.
+        // Click on "Continue as a guest".
+        // Proceed through the upcoming 3 steps by clicking the green button.
+        // Select "Trip type, one way".
+        // Click on the departure country option and remove the default country.
+        // Enter the departure country/city and click on "select".
+        // Click on the arrival country option and enter the destination country.
+        // Select the departure date as September 21 and click on "set date".
+        // Click on the "search" button.
+        // Apply filters for the cheapest and non-stop options.
+        // Save the incoming ticket price and send it to the user's phone as an SMS.
+    }
+}
